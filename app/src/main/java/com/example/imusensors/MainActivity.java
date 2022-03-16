c686169839b1ddfb7c657c80370243695409223a
demon <demonsd98@outlook.com>
2022-03-16 20:54:32 +0000

    Download ready. Read and write file error.

--- app/src/main/java/com/example/imusensors/MainActivity.java
+++ app/src/main/java/com/example/imusensors/MainActivity.java
@@ -4,18 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Objects;
 
 import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,8 +58,10 @@ public class MainActivity extends AppCompatActivity
 
     private final String TAG = "IMUSensorLog";
     private boolean idcWrite; // indicator true to start, false to stop
-    File dataFile;
-    FileOutputStream fileOutputStream;
+    private File dataFile;
+    private FileOutputStream fileOutputStream;
+    private String currentDataPath;
+    private String dataFileName;
 
 
     @Override
@@ -83,6 +96,8 @@ public class MainActivity extends AppCompatActivity
         idcWrite = false;
         dataFile = null;
         fileOutputStream = null;
+        currentDataPath = "";
+        dataFileName = "";
     }
 
     @Override
@@ -242,17 +257,28 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
+//        Uri currentDataUri = Uri.fromFile(dataFile);
+//        DownloadManager.Request request = new DownloadManager.Request(currentDataUri);
+//
+//        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "fileName");
+//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); // to notify when download is complete
+//        request.allowScanningByMediaScanner();// if you want to be available from media players
+//        DownloadManager manager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+//        manager.enqueue(request);
+
+        createExtFile(Uri.fromFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
+
         idcWrite = false;
         dataFile = null;
         fileOutputStream = null;
+        currentDataPath = "";
+        dataFileName = "";
     }
 
 
-    private String currentDataPath;
-
     private File createDataFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        String dataFileName = "IMUData_" + timeStamp; // file name
+        dataFileName = "IMUData_" + timeStamp; // file name
         File mAppStorageDir = getFilesDir(); // internal storage directory
         File imuData = File.createTempFile(
                 dataFileName,
@@ -281,6 +307,61 @@ public class MainActivity extends AppCompatActivity
                 e.printStackTrace();
             }
         }
+    }
+
+    // Request code for creating a PDF document.
+    private static final int CREATE_FILE = 1;
+
+    private void createExtFile(Uri pickerInitialUri) {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TITLE, dataFileName);
+
+        // Optionally, specify a URI for the directory that should be opened in
+        // the system file picker when your app creates the document.
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, CREATE_FILE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CREATE_FILE) {
+            if (resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                try {
+                    OutputStream outputStream = getContentResolver().openOutputStream(uri);
+                    outputStream.write(readTextFromUri(Uri.fromFile(dataFile))
+                            .getBytes(StandardCharsets.UTF_8));
+                    outputStream.close();
+                    Toast.makeText(this, "File saved.", Toast.LENGTH_SHORT).show();
+                }
+                catch (IOException e) {
+                    Toast.makeText(this, "Save file failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                Toast.makeText(this, "File not saved.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                getContentResolver().openInputStream(uri);
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
     }
 
 
